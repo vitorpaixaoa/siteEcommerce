0ca7bb43cde5881cbd2e74ca4c5a8763fad41d5b
vitorpaixaoa <vitorpaixaoa.loubi@gmail.com>
2020-12-09 11:43:41 -0300

    configurando

---
+++ config.js
@@ -0,0 +1,4 @@
+export const API = "http://localhost:3000",
+             versao = "v1",
+             loja = "5fbbd448fc2dc31564609d99",
+             base = API + "/public/images/";
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Link from 'next/link'
+
+export default class Index extends Component {
+
+    render(){
+        return(
+            <div>
+                <h2> LOJA TI INDEX </h2>
+            </div>
+        )
+    }
+}
