3e239a8eb31affa231e49cd6f32db6bdc9b843b0
Alan Vitor Paixao Almeida <vitorpaixao@codions.com>
2021-09-02 23:48:24 -0300

    Correcao

--- redux/actions/carrinhoActions.js
+++ redux/actions/carrinhoActions.js
@@ -1,3 +1,4 @@
+  
 import axios from 'axios';
 import { API, versao, loja } from '../../config';
 import { getCart, removeCart, cleanCart } from '../../utils/cart';
--- redux/reducers/carrinhoReducer.js
+++ redux/reducers/carrinhoReducer.js
@@ -10,18 +10,17 @@ import {
     CLEAN_FRETES
 } from '../types';
 
-
 const initialState = { carrinho: null };
 
-export default ( state = initialState, action ) => {
+export default (state = initialState, action) => {
     switch(action.type){
         case SET_CARRINHO:
-            return{
+            return {
                 ...state,
                 carrinho: action.carrinho
             }
         case CLEAN_CARRINHO:
-            return{
+            return {
                 ...state,
                 carrinho: null,
                 freteSelecionado: null,
@@ -30,51 +29,53 @@ export default ( state = initialState, action ) => {
             }
         case FETCH_PRODUTO_CARRINHO:
             if(!action.payload.produto) return state;
-            return{
+            return {
                 ...state,
                 carrinho: state.carrinho ? state.carrinho.map((item, index) => {
-                    return ( action.idxCarrinho === index ) ?
-                        { ...item, produto: action.payload.produto } : 
+                    return ( action.idxCarrinho == index ) ?
+                        { ...item, produto: action.payload.produto } :
                         item
                 }) : []
             }
         case FETCH_VARIACAO_CARRINHO:
             if(!action.payload.variacao) return state;
-            return{
+            return {
                 ...state,
                 carrinho: state.carrinho ? state.carrinho.map((item, index) => {
-                    return ( action.idxCarrinho === index ) ?
-                        { ...item, variacao: action.payload.variacao } : 
+                    return ( action.idxCarrinho == index ) ?
+                        { ...item, variacao: action.payload.variacao } :
                         item
                 }) : []
             }
         case FETCH_VALOR_ENTREGA:
-            return{
+            return {
                 ...state,
                 fretes: action.payload.resultados,
                 freteSelecionado: action.payload.resultados[0],
                 cep: action.cep
             }
         case UPDATE_QTD_CART:
-            return{
+            return {
                 ...state,
                 carrinho: state.carrinho ? state.carrinho.map((item, index) => {
-                    return ( action.idxCarrinho === index ) ?
-                        { ...item, 
-                            quantidade: Number(item.quantidade) + Number(action.change) } : 
+                    return ( action.idxCarrinho == index ) ?
+                        { 
+                            ...item, 
+                            quantidade: Number(item.quantidade) + Number(action.change)
+                        } :
                         item
                 }) : []
             }
         case REMOVE_PROD_CART:
-            return{
+            return {
                 ...state,
                 carrinho: state.carrinho.reduce(
-                    (all, item, index)  =>
-                    index !== action.idxCarrinho ? all.concat([item]) : all, []
-                )
+                        (all, item, index) => 
+                        index !== action.idxCarrinho ? all.concat([item]) : all, []
+                    )
             }
         case UPDATE_FRETE_CART:
-            return{
+            return {
                 ...state,
                 freteSelecionado: action.freteSelecionado
             }
